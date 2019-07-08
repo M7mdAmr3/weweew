a0f2e989f44db76b61be3440d3e90b12182c54b5
M7mdAmr3 <43708078+M7mdAmr3@users.noreply.github.com>
2019-07-08 12:02:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', function(){
  game: { 
     type: 1,
      url: 'https://www.twitch.tv/itsfakehehe',
-    name: 'I'm ALWAYS SAD...',
+    name: 'I`m ALWAYS SAD...',
     application_id: '595981556963737611',
      assets: {
          large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
